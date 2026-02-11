5e6e07df8c143ae60ca96c32209d063f88789334
uma-sketch <main.vertex5@gmail.com>
2026-02-11 22:18:46 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,15 +1,15 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
-  tags     = var.common_tags
+  tags     = local.required_tags
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-appgw"
-  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  tags     = var.common_tags
+  address_space       = ["10.0.0.0/16"]
+  tags                = local.required_tags
 }
 
 resource "azurerm_subnet" "appgw" {
