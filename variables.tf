764d4128fe0791991ccd5893de7ed555439f497e
uma-sketch <main.vertex5@gmail.com>
2026-02-11 09:02:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "location" { default = "Central India" }
 variable "rg_name"  { default = "rg-appgw" }
 variable "vm_count" { default = 2 }
+
+variable "common_tags" {
+  type = map(string)
+  default = {
+    Environment = "Dev"
+    Owner       = "Umashankar"
+  }
+}
